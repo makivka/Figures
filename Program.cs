c3a120cb284445a825d4d7a4eec1d2197fcc87d3
makivka79@gmail.com <makivka79@gmail.com>
2022-08-10 14:51:30 +0300

    +

--- Program.cs
+++ Program.cs
@@ -6,7 +6,16 @@ namespace Figures
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Circle circle1= new Circle
+
+            { 
+                Name= "Circle 5",
+                Color="Red",
+                Radius=5
+                 
+            };
+            
+            circle1.Draw();
         }
     }
 }
---
+++ Shape.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace Figures
+{
+    abstract class Shape
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public abstract void Draw();
+    }
+}
