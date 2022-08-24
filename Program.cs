22d1f336167ab423d5ca856658243a7b5b387fdd
makivka79@gmail.com <makivka79@gmail.com>
2022-08-24 13:37:24 +0300

    void Draw() for Rectangle updated

--- Program.cs
+++ Program.cs
@@ -8,16 +8,17 @@ namespace Figures
         static void Main(string[] args)
         {
             List<Shape> shapes = new ();
-            shapes.Add ( new Circles("Red", 5));
-            shapes.Add ( new Rectangle ("Red", new double [4,2] { { 1, 3 }, { 5, 3 }, { 1, 8 }, { 5, 8 } }));
-            shapes.Add ( new Circles("Blue", 6));
+            shapes.Add ( new Circles("Red", 2));
+            shapes.Add ( new Rectangle ("Red", new double [4,2] { { 1, 3 }, { 9, 3 }, { 1, 10 }, { 9, 10 } }));
+            shapes.Add ( new Circles("Blue", 5));
             //foreach (Shape sh in shapes)
             // {
             //     Console.WriteLine(sh);
             //     sh.Draw();
             // }
             shapes[0].Draw();
-            
+            shapes[2].Draw();
+            shapes[1].Draw();
         }
     }
 }
--- Rectangle.cs
+++ Rectangle.cs
@@ -26,15 +26,46 @@ namespace Figures
         }
          public override void Draw()
         {
-            for (int i = Convert.ToInt32(Coordinates[0, 0]); i <= Convert.ToInt32(Coordinates[1, 0]); i++)
-            {
-                for (int j = Convert.ToInt32(Coordinates[0, 1]); j <= Convert.ToInt32(Coordinates[2, 1]); j++)
-                {
-                    Console.SetCursorPosition(i, j);
-                    Console.Write("*");
 
+            int x1 = Convert.ToInt32(Coordinates[0, 0]);
+            int x2 = Convert.ToInt32(Coordinates[1, 0]);
+            int y1 = Convert.ToInt32(Coordinates[0, 1]);
+            int y2 = Convert.ToInt32(Coordinates[2, 1]);
+
+            int numberX = x2-x1;
+            int numberY = y2 - y1;
+            for (int i = 0; i <= numberY; i++)
+            {
+                if (i == 0 || i == numberY)
+                {
+                    for (int j = 0; j < numberX; j++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+                if (i >= 1 && i <= numberY-1)
+                {
+                    for (int j = 0; j <= numberX; j++)
+                    {
+                        if (j == 0 || j == numberX)
+                        {
+                            Console.Write("*");
+                        }
+                        else if (j >= 1 && j <= numberX-1)
+                        {
+                            Console.Write(" ");
+                        }
+                    }
+                    Console.WriteLine();
                 }
             }
+            
         }
+           
+            
+
+
+        
     }
  }
